e8df106c7214afb8fab33c1ac202a09fbde42273
MochiMochinoMi <aissaouiyounes56@gmail.com>
2023-07-09 22:45:01 +0100

    hey

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Timgad informatique\Desktop\MARK EXCEL WEP APP\venv
+set VIRTUAL_ENV=C:\Users\Timgad informatique\Desktop\UPUPEXCEL\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
