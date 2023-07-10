2a5042cb9113492eb3eb015abff62fbbcf1588a6
MochiMochinoMi <aissaouiyounes56@gmail.com>
2023-07-10 20:57:59 +0100

    hryy

---
+++ website/views.py
@@ -0,0 +1,118 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for,flash
+import pandas as pd
+import os
+from .logic import extract_zip, stack_excel_data, plot_stock_data,plot_single_daily_data,is_valid_date
+import matplotlib.pyplot as plt
+import threading
+import glob
+from flask import render_template, request, session, redirect, url_for,flash
+import time
+from flask import redirect, url_for
+from datetime import datetime
+import xlrd
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        uploaded_file = request.files['file']
+        file_path = "uploaded_file.zip"
+        uploaded_file.save(file_path)
+        # Extract the ZIP file
+        extracted_folder = extract_zip(file_path)
+        # Construct the full folder path
+        folder_path = os.path.join(extracted_folder)
+        session['folder_path'] = folder_path
+        print("Folder Path:", folder_path)
+        return redirect(url_for('views.graph_selection'))
+
+    return render_template('base.html')
+
+def plot_graph(folder_path, start_date, end_date, duration, stock_names,variable,log_scale):
+
+    data = stack_excel_data(folder_path)
+    data.index = data.index.date
+
+    plot_stock_data(data, start_date, end_date, duration, stock_names,variable,log_scale)
+
+    # Set a session variable with the warning message
+
+def plot_graph2(folder_path, fixed_date, ticker,log_scale):
+
+    # Process the fixed_date input as a datetime object
+    time= datetime.strptime(fixed_date, "%Y-%m-%d").date()
+    data = stack_excel_data(folder_path)
+    data.index = data.index.date
+
+    plot_single_daily_data(data, ticker, time,log_scale)
+
+
+
+
+
+
+
+@views.route('/graph_selection', methods=['GET', 'POST'])
+def graph_selection():
+
+    if request.method == 'POST':
+        
+        plot_type = request.form['plot_type']
+        if plot_type == 'plot_any_stock':
+                log_scale = False  # Default value
+                folder_path = session.get('folder_path')
+                start_date = request.form['start_date']
+                end_date = request.form['end_date']
+                duration = request.form['duration']
+                stock_names = request.form['stock_names']
+                stock_names = [name.strip() for name in stock_names.split(',')]
+                variable = request.form['variable']
+                log_scale = 'log_scale' in request.form
+                # Check if form data is valid
+                if not is_valid_date(start_date) or not is_valid_date(end_date):
+                    flash('Please enter a valid date in the format YYYY-MM-DD.')
+                    return render_template('graph_selection.html')
+                start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+                end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                graph_thread = threading.Thread(target=plot_graph, args=(session.get('folder_path'), start_date, end_date, duration, stock_names,variable,log_scale))
+                graph_thread.start()
+                plot_file_name = os.path.join(os.getcwd(),'temp', 'stock_data_plot0.jpg')
+                return redirect(url_for('views.show_graph', plot_path=plot_file_name))
+            
+        elif plot_type == 'plot_daily_change':
+            log_scale = False  # Default value
+            folder_path = session.get('folder_path')
+            time = request.form['fixed_date']
+            ticker = request.form['ticker']
+            log_scale = 'log_scale' in request.form
+            if not is_valid_date(time):
+                flash('Please enter a valid date in the format YYYY-MM-DD.')
+                return render_template('graph_selection.html')
+            graph_thread = threading.Thread(target=plot_graph2, args=(session.get('folder_path'), time, ticker,log_scale))
+            graph_thread.start()
+            plot_file_name = os.path.join(os.getcwd(),'temp', 'stock_data_plot1.jpg')
+            return redirect(url_for('views.show_graph', plot_path=plot_file_name))
+
+    delete_cached_images()
+    return render_template('graph_selection.html')
+
+
+
+
+from flask import send_from_directory
+
+@views.route('/show_graph/<path:plot_path>')
+def show_graph(plot_path):
+    directory = os.path.dirname(plot_path)
+    filename = os.path.basename(plot_path)
+    time.sleep(5)
+    return send_from_directory(directory, filename, as_attachment=False)
+
+
+def delete_cached_images():
+    static_folder = os.path.join(os.getcwd(), 'temp')
+    cached_images = glob.glob(os.path.join(static_folder, 'stock_data_plot*.jpg'))
+    for image_path in cached_images:
+        os.remove(image_path)
